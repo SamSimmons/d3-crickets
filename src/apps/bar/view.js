fb1824918add1b5214b76d19eadb4eb14c6591c4
Sam Simmons <samuel.j.simmons@gmail.com>
2016-08-02 15:49:44 +1200

    add yAxis to bars

--- src/apps/bar/view.js
+++ src/apps/bar/view.js
@@ -27,20 +27,20 @@ const BarView = Marionette.ItemView.extend({
     }
     const width = this.el.clientWidth
     const height = this.el.clientHeight
-    const barWidth = (width - margin.left - margin.right) / data.length
     const yScale = d3.scaleLinear().range([height - margin.top - margin.bottom, 0]).domain([0, d3.max(data)])
     const xScale = d3.scaleBand()
       .domain(data)
       .rangeRound([0, width - margin.left - margin.right])
       .paddingInner(0.2)
 
-    return { data, width, height, margin, barWidth, xScale, yScale }
+    return { data, width, height, margin, xScale, yScale }
   },
   renderGraph: function () {
-    const { data, width, height, margin, barWidth, xScale, yScale } = this.graph
+    const { data, width, height, margin, xScale, yScale } = this.graph
 
     const xAxisScale = d3.scaleBand().domain(data.map((x, i) => i + 1)).rangeRound([0, width - margin.left - margin.right]).paddingInner(0.2)
     const xAxis = d3.axisBottom().scale(xAxisScale)
+    const yAxis = d3.axisLeft().scale(yScale)
 
     const chart = d3.select(this.ui.canvas[0]).append('g')
       .attr('transform', `translate(${margin.left}, ${margin.top})`)
@@ -56,17 +56,13 @@ const BarView = Marionette.ItemView.extend({
       .attr('width', xScale.bandwidth())
       .style('fill', 'steelblue')
 
-    // bar.append('text')
-    //   .attr('x', xScale.bandwidth() / 2)
-    //   .attr('y', d => yScale(d) + 3)
-    //   .attr('dy', '.75em')
-    //   .text(d => d)
-
     chart.append('g')
       .attr('transform', `translate(0, ${height - margin.top - margin.bottom})`)
       .call(xAxis)
 
-
+    chart.append('g')
+      .attr('transform', `translate(0, ${margin.top - margin.bottom})`)
+      .call(yAxis)
 
   }
 })
